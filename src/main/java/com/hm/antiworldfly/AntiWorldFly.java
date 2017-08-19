d8d645385fbb8fa451922bf80dc275c488e94f5b
PyvesB <PyvesDev@gmail.com>
2017-08-19 13:24:39 +0100

    Increased version number to 2.3.2

--- src/main/java/com/hm/antiworldfly/AntiWorldFly.java
+++ src/main/java/com/hm/antiworldfly/AntiWorldFly.java
@@ -39,7 +39,7 @@ import com.hm.mcshared.update.UpdateChecker;
  * Spigot project page: spigotmc.org/resources/anti-world-fly.5357
  * 
  * @since March 2015.
- * @version 2.3.1
+ * @version 2.3.2
  * @author DarkPyves
  */
 
