8c78275e1bab2ac3f497978a02824055592675da
Sidpatchy <35241490+Sidpatchy@users.noreply.github.com>
2021-06-29 22:04:05 -0500

    Bump version to v2.4.1

--- src/main/java/com/hm/antiworldfly/AntiWorldFly.java
+++ src/main/java/com/hm/antiworldfly/AntiWorldFly.java
@@ -35,7 +35,7 @@ import com.hm.mcshared.update.UpdateChecker;
  * Spigot project page: spigotmc.org/resources/anti-world-fly.5357
  * 
  * @since March 2015.
- * @version 2.4.0
+ * @version 2.4.1
  * @author DarkPyves
  * @maintainer  Sidpatchy
  */
